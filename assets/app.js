246c00a0da50ea0705f1cf9d4a933cd0e8f608ed
dodji05 <gildas31@gmail.com>
2024-08-03 15:34:30 +0100

    beta-1

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,26 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/bootstrap.min.css';
+import './styles/magnific-popup.css';
+import './styles/materialdesignicons.min.css';
+import './styles/materialdesignicons.min.css';
+import './styles/pe-icon-7.css';
+import './styles/owl.carousel.css';
+import './styles/owl.theme.css';
+import './styles/owl.transitions.css';
+import './styles/style.css';
+
+import './js/jquery.min.js';
+import './js/popper.min.js';
+import './js/bootstrap.bundle.min.js';
+import './js/jquery.easing.min.js';
+import './js/scrollspy.min.js';
+import './js/owl.carousel.min.js';
+import './js/jquery.magnific-popup.min.js';
+import './js/custom.js';
+
+
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
